cf68818ff33d19bc2206d83765b1a334392983d1
LAPTOP-461AOIA4\User <victor920617@gmail.com>
2023-11-25 22:10:38 +0800

    更改addItem，加入數量變化

--- HW3.a__shopping/customer/shoppingModel.php
+++ HW3.a__shopping/customer/shoppingModel.php
@@ -29,13 +29,28 @@ function getCartList() {
 }
 
 function addItem($id,$name,$price) {
-	global $db;
-
-	$sql = "insert into customer (id, goods, price) values (?, ?, ?)"; //SQL中的 ? 代表未來要用變數綁定進去的地方
-	$stmt = mysqli_prepare($db, $sql); //prepare sql statement
-	mysqli_stmt_bind_param($stmt, "isi",$id,$name,$price); //bind parameters with variables, with types "sss":string, string ,string
-	mysqli_stmt_execute($stmt);  //執行SQL
-	return True;
+    global $db;
+    $sql = "select * from customer where id= $id;";
+    $stmt = mysqli_prepare($db, $sql); //prepare sql statement
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt); //取得查詢結果
+    $row=mysqli_fetch_assoc($result);
+    if ($row) {
+        $newAmount = $row['amount'] + 1;
+        $newTotal = $newAmount * $row['price'];
+        $updateSql = "UPDATE customer SET amount = ?, total = ? WHERE id = ?";
+        $updateStmt = mysqli_prepare($db, $updateSql);
+        mysqli_stmt_bind_param($updateStmt, "iii", $newAmount, $newTotal, $id);
+        mysqli_stmt_execute($updateStmt);
+    }
+    else {
+		$amount=1;
+		$sql = "insert into customer (id, goods, price, amount, total) values (?, ?, ?, ?, ?)"; //SQL中的 ? 代表未來要用變數綁定進去的地方
+		$stmt = mysqli_prepare($db, $sql); //prepare sql statement
+		mysqli_stmt_bind_param($stmt, "isiii",$id,$name,$price,$amount,$price); //bind parameters with variables, with types "sss":string, string ,string
+		mysqli_stmt_execute($stmt);  //執行SQL
+    }
+    return True;
 }
 
 
