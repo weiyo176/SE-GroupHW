36285ad89348595c16aca61a4336651c28fe7887
MSI\z0901 <z0901234720@gmail.com>
2023-11-25 15:30:34 +0800

    新增business

--- HW3.a__shopping/customer/shoppingModel.php
+++ HW3.a__shopping/customer/shoppingModel.php
@@ -48,4 +48,47 @@ function delItem($id) {
 	mysqli_stmt_execute($stmt);  //執行SQL
 	return True;
 }
+function getJobList() {
+	global $db;
+	$sql = "select * from items;";
+	$stmt = mysqli_prepare($db, $sql ); //precompile sql指令，建立statement 物件，以便執行SQL
+	mysqli_stmt_execute($stmt); //執行SQL
+	$result = mysqli_stmt_get_result($stmt); //取得查詢結果
+
+	$rows = array(); //要回傳的陣列
+	while($r = mysqli_fetch_assoc($result)) {
+		$rows[] = $r; //將此筆資料新增到陣列中
+	}
+	return $rows;
+}
+
+function addJob($name,$price,$description,$jobID) {
+	global $db;
+	if($jobID>0) {
+		$sql = "update items set name=?, price=?, description=? where id=?"; //SQL中的 ? 代表未來要用變數綁定進去的地方
+		$stmt = mysqli_prepare($db, $sql); //prepare sql statement
+		mysqli_stmt_bind_param($stmt, "sssi", $name, $price,$description,$jobID); //bind parameters with variables, with types "sss":string, string ,string
+	} else {
+		$sql = "insert into items (name, price, description) values (?, ?, ?)"; //SQL中的 ? 代表未來要用變數綁定進去的地方
+		$stmt = mysqli_prepare($db, $sql); //prepare sql statement
+		mysqli_stmt_bind_param($stmt, "sss", $name, $price,$description); //bind parameters with variables, with types "sss":string, string ,string
+	}
+	mysqli_stmt_execute($stmt);  //執行SQL
+	return True;
+}
+
+function updateJob($id, $name,$price,$description) {
+	echo $id, $name,$price,$description;
+	return;
+}
+
+function delJob($id) {
+	global $db;
+
+	$sql = "delete from items where id=?;"; //SQL中的 ? 代表未來要用變數綁定進去的地方
+	$stmt = mysqli_prepare($db, $sql); //prepare sql statement
+	mysqli_stmt_bind_param($stmt, "i", $id); //bind parameters with variables, with types "sss":string, string ,string
+	mysqli_stmt_execute($stmt);  //執行SQL
+	return True;
+}
 ?>
