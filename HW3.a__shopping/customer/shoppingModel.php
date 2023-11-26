0bca9caeb5ae3c4c27238bd722fb4114024d96a5
LAPTOP-461AOIA4\User <victor920617@gmail.com>
2023-11-26 20:55:54 +0800

    新增總價

--- HW3.a__shopping/customer/shoppingModel.php
+++ HW3.a__shopping/customer/shoppingModel.php
@@ -20,11 +20,17 @@ function getCartList() {
 	$stmt = mysqli_prepare($db, $sql ); //precompile sql指令，建立statement 物件，以便執行SQL
 	mysqli_stmt_execute($stmt); //執行SQL
 	$result = mysqli_stmt_get_result($stmt); //取得查詢結果
-
+	$totalPrice=0;
 	$rows = array(); //要回傳的陣列
 	while($r = mysqli_fetch_assoc($result)) {
+		$totalPrice += $r['total'];
 		$rows[] = $r; //將此筆資料新增到陣列中
 	}
+	$newData = array(
+		'goods' => 'TotalPrice',
+		'total' => $totalPrice
+	);
+	$rows[] = $newData;
 	return $rows;
 }
 
