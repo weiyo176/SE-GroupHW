1c9f8ce0271eba61702be62469be547905c1ec8a
LAPTOP-461AOIA4\User <victor920617@gmail.com>
2023-12-30 02:29:47 +0800

    下一個SQL結構

---
+++ HW3.a__shopping/customer/mvc_c.sql
@@ -0,0 +1,156 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2023-12-29 19:27:27
+-- 伺服器版本： 10.4.28-MariaDB
+-- PHP 版本： 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `mvc_c`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `business`
+--
+
+CREATE TABLE `business` (
+  `bIID` int(11) NOT NULL,
+  `name` varchar(10) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `password` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 傾印資料表的資料 `business`
+--
+
+INSERT INTO `business` (`bIID`, `name`, `email`, `password`) VALUES
+(1, 'superman', '123@123', '123');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `customer`
+--
+
+CREATE TABLE `customer` (
+  `cID` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
+  `oID` int(11) NOT NULL,
+  `goods` varchar(20) NOT NULL,
+  `price` int(11) NOT NULL,
+  `amount` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `status` varchar(3) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '未處理',
+  `rating` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `customers`
+--
+
+CREATE TABLE `customers` (
+  `cID` int(11) NOT NULL,
+  `name` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `items`
+--
+
+CREATE TABLE `items` (
+  `bID` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
+  `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 傾印資料表的資料 `items`
+--
+
+INSERT INTO `items` (`bID`, `id`, `name`, `description`, `price`) VALUES
+(1, 1, 'apple', 'aaa', 10),
+(1, 2, 'banana', 'bbb', 20);
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `business`
+--
+ALTER TABLE `business`
+  ADD PRIMARY KEY (`bIID`);
+
+--
+-- 資料表索引 `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`cID`);
+
+--
+-- 資料表索引 `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`cID`);
+
+--
+-- 資料表索引 `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `business`
+--
+ALTER TABLE `business`
+  MODIFY `bIID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `cID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `cID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `items`
+--
+ALTER TABLE `items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
